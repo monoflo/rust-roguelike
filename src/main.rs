7bbb500be8003bf273d90871ebfd2a70e9dd98ff
monoflo <16790838+monoflo@users.noreply.github.com>
2022-07-08 00:02:49 -0400

    Complete Tutorial Part 1: Graphics
    https://tomassedovic.github.io/roguelike-tutorial/part-1-graphics.html

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const LIMIT_FPS: i32 = 20;
+const SCREEN_HEIGHT: i32 = 50;
+const SCREEN_WIDTH: i32 = 80;
+
+struct Tcod {
+    root: Root,
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    match tcod.root.wait_for_keypress(true) {
+        // fullscreen
+        Key { code: Enter, alt: true, .. } => tcod.root.set_fullscreen(!tcod.root.is_fullscreen()),
+
+        // exit game
+        Key { code: Escape, .. } => return true,
+
+        // movement
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        _ => {},
+    }
+
+    false
+}
+
+fn main() {
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let mut tcod = Tcod { root };
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.root.flush();
+
+        if handle_keys(&mut tcod, &mut player_x, &mut player_y) {
+            break;
+        }
+    }
+}
